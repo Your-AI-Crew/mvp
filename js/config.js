5d7b5c60d4406350b75db694d41a798134242d51
Your-AI-Crew <sab993531@gmail.com>
2026-01-01 17:40:31 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 // js/config.js
 export const CONFIG = {
-  WEBHOOK_URL: 'https://REPLACE_WITH_N8N_WEBHOOK',
+  WEBHOOK_URL: 'https://cmew.app.n8n.cloud/webhook/6601e59c-1e1d-4bbc-9bc3-1b283d7268e8',
   DEBUG: false
+
 };
